2080d0147cc6bae93f3a32d7105a487c1c81eb7a
V-KMilev <vilimirmilev@gmail.com>
2020-12-15 23:28:01 +0200

    Working Update -!getcodes -hotfix

--- src/com/appnull/commands/ScheduleNotofications.java
+++ src/com/appnull/commands/ScheduleNotofications.java
@@ -186,6 +186,40 @@ public class ScheduleNotofications extends ListenerAdapter {
 			event.getChannel().sendMessage(eb.build()).queue();
 		}
 
+		if (message.contentEquals("!getcodes")) {
+
+			EmbedBuilder eb = new EmbedBuilder();
+
+			eb.setTitle("CraftCN - HELP");
+			eb.addField("> **йю**", "**йнд: ||hzxzzjo||**", true);
+			eb.addField("> **со-йю ЦП.1**", "**йнд: ||vb99omq||**", true);
+			eb.addField("> **со-йю ЦП.2**", "**йнд: ||3341wao||**", true);
+			eb.addField("> **жя**", "**йнд: ||ew6szka||**", true);
+			eb.addField("> **со-яУЕЛ ЦП.1**", "**йнд: ||4vtm2fj||**", true);
+			eb.addField("> **со-яУЕЛ ЦП.2**", "**йнд: ||1znwjk1||**", true);
+			eb.addField("> **гос**", "**йнд: ||3m9tmd5||**", true);
+			eb.addField("> **со-гос ЦП.1**", "**йнд: ||ky2j7rs||**", true);
+			eb.addField("> **со-гос ЦП.2**", "**йнд: ||6nstghk||**", true);
+			eb.addField("> **ня**", "**йнд: ||6sgsil2||**", true);
+			eb.addField("> **со-ня ЦП.1**", "**йнд: ||8oumx66||**", true);
+			eb.addField("> **со-ня ЦП.2**", "**йнд: ||6sgsil2||**", true);
+			eb.addField("> **йл**", "**йнд: ||gyix9kd||**", true);
+			eb.addField("> **люр**", "**йнд: ||fcs8b64||**", true);
+			eb.addField("> **вео**", "**йнд: ||vva8xiv||**", true);
+			eb.addField("> **аек**", "**йнд: ||b2x1o8k||**", true);
+			eb.addField("> **ме**", "**йнд: ||r3g6lg3||**", true);
+			eb.addField("> **цн**", "**йнд: ||pkiaoay||**", true);
+			eb.addField("> **яд**", "**йнд: ||mcsehdp||**", true);
+			eb.addField("> **тбя**", "**йнд: ||mcsehdp||**", true);
+			eb.setFooter("! For more help CurrentlyNull#3126 or vilimir.k.milev@gmail.com");
+
+			eb.setColor(Color.RED);
+			eb.setThumbnail(
+					"https://cdn.discordapp.com/attachments/710039794884935743/785396990242455613/haah_-_Copy_-_Copy_-_Copy.png");
+
+			event.getChannel().sendMessage(eb.build()).queue();
+		}
+
 		if (message.contentEquals("!kys") || message.contentEquals("!KYS")) {
 
 			event.getChannel().sendMessage("NO! KYS " + event.getMember().getAsMention()).queue();
